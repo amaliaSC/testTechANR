58647343a1ed40922b8fbdd150d8c3b3577f7be4
amaliaSC <amaliasabou@yahoo.fr>
2024-04-22 13:53:54 +0200

    user role/admin role done

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,8 +11,12 @@ class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
+            $admin = ["ROLE_ADMIN", "ROLE_USER"];
+            $user = [];
 
-        $this->getUser();
+
+
+       // $this->getUser();
         return $this->render('home_page/index.html.twig', [
             
         ]);
